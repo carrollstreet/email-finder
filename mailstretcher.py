c160a1d0a6f6adf3ae8095a6278b49d2badf4564
Kiryl <musomania@protonmail.com>
2020-11-04 14:25:52 +0300

    version 2.0
    also looking for emails in commits

--- mailstretcher.py
+++ mailstretcher.py
@@ -12,8 +12,24 @@ def gitapimails():
     raw = list(map(lambda x: x.replace('"email":', '').strip('""'), raw))
     mails = ', '.join(set(raw))
     print(f'Emails from github public API: {mails}')
-    
+    try:
+        start_url = 'https://github.com/' + nick + '?tab=repositories'
+        r = requests.get(start_url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        repolink = []
+        for i in soup.find_all('div', class_="d-inline-block mb-1"):
+            if 'Forked' not in str(i.find('span')):
+                repolink.append('https://github.com' + i.find('a').get('href'))
+        repolink_first_commit = str(str(repolink[0]) + '/commits/master')
+        rep_comm = requests.get(repolink_first_commit)
+        soup = BeautifulSoup(rep_comm.text, 'lxml')
+        commit = re.findall('/commit/[\w]+', str(soup.find_all('a')))[0]
+        commit_url = str(repolink[0]) + commit + '.patch'
+        req_com = requests.get(commit_url)
+        mails = re.findall('<[\w@\w.\w]+>',req_com.text)
+        print('Email from commit.patch:', ''.join(mails).strip('<>'))
+    except:
+        pass
 def retry():
     return gitapimails(), retry()
 retry()
-
