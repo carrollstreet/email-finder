181e152c07fcd56c59bba0021e8bbcf4b21ffe4b
Kiryl <musomania@protonmail.com>
2020-11-04 14:27:13 +0300

    version 3.0

--- mailstretcher.py
+++ mailstretcher.py
@@ -4,14 +4,22 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import time
+
 def gitapimails():
     nick = input()
+    start = time.time()
     url = 'https://api.github.com/users/' + nick + '/events/public'
     req = requests.get(url)
-    raw = re.findall('"email":"[\w]+@[\w.\w]+"', req.text)
+    raw = re.findall('"email":"[\w.]+@[\w.]+"', req.text)
     raw = list(map(lambda x: x.replace('"email":', '').strip('""'), raw))
-    mails = ', '.join(set(raw))
-    print(f'Emails from github public API: {mails}')
+    mails = set(raw)
+    print()
+    if len(mails) > 0:
+        print('Emails from github public API:', ', '.join(mails))
+    else:
+        print('There are no emails in github pubplic API for this profile')
+    print('Looking for emails in repositories, wait please')
     try:
         start_url = 'https://github.com/' + nick + '?tab=repositories'
         r = requests.get(start_url)
@@ -20,16 +28,30 @@ def gitapimails():
         for i in soup.find_all('div', class_="d-inline-block mb-1"):
             if 'Forked' not in str(i.find('span')):
                 repolink.append('https://github.com' + i.find('a').get('href'))
-        repolink_first_commit = str(str(repolink[0]) + '/commits/master')
-        rep_comm = requests.get(repolink_first_commit)
-        soup = BeautifulSoup(rep_comm.text, 'lxml')
-        commit = re.findall('/commit/[\w]+', str(soup.find_all('a')))[0]
-        commit_url = str(repolink[0]) + commit + '.patch'
-        req_com = requests.get(commit_url)
-        mails = re.findall('<[\w@\w.\w]+>',req_com.text)
-        print('Email from commit.patch:', ''.join(mails).strip('<>'))
+        repolink_first_commit = [str(i) + '/commits/master' for i in repolink]
+        mail_list = []
+        for k, i in enumerate(repolink_first_commit):
+            try:
+                rep_comm = requests.get(i)
+                soup = BeautifulSoup(rep_comm.text, 'lxml')
+                commit = re.findall('/commit/[\w]+', str(soup.find_all('a')))[0]
+                commit_url = str(repolink[k]) + commit + '.patch'
+                req_com = requests.get(commit_url)
+                mails = re.findall('<[\w.]+@[\w.]+>',req_com.text)[0]
+                mail_list.append(''.join(mails).strip('<>'))
+            except:
+                pass
+        if len(mail_list) > 0:
+            print('Emails from commit.patch:', ', '.join(set(mail_list)))
+        else:
+            print("Can't find any emails in repositories")
     except:
+        print('Error')
         pass
+    print('Emails search took {:.2f} seconds'.format(time.time()- start))
+    print()
+
 def retry():
     return gitapimails(), retry()
+
 retry()
